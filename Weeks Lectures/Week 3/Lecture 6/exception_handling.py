8cb7325e4fcb40311269a47cd761f4708451cf1e
BatoulSbat <batoul.sbat99@gmail.com>
2024-04-06 03:53:10 +0100

    Third commit

---
+++ Weeks Lectures/Week 3/Lecture 6/exception_handling.py
@@ -0,0 +1,26 @@
+# #try, except, else, finally
+# #x = 2
+# try:
+#     print(x)
+# except NameError:
+#     print("Variable x  is not defined.")
+# else:
+#     #except:
+#     print("Nothing went wrong.")
+# #if x is defined then  it will print the value of x otherwise it will print "An exception occurred."
+# finally:
+#     print("The 'try except' block is completed.")
+
+# #raise an error
+# y = -1
+
+# if y < 0:
+#     raise Exception("Sorry Batoul, no numbers below zero")
+
+# z = 1.2
+
+# if not type(z) is int: #this is the same as if not isinstance(z, int): and this method will check if the value of z is the same as defined in the next parameter which is in this case integer
+#     raise Exception("Only integers are allowd Yoda")
+
+
+print('This is the end.')
