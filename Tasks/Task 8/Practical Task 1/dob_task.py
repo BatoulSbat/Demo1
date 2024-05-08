8fc0979bea7eaf20af8bd1f8cb277e184068d835
BatoulSbat <batoul.sbat99@gmail.com>
2024-05-08 22:03:40 +0100

    18th commit

--- Tasks/Task 8/Practical Task 1/dob_task.py
+++ Tasks/Task 8/Practical Task 1/dob_task.py
@@ -8,12 +8,23 @@ birthdates = []
 
 # Iterate through each line of the file
 for line in lines:
-    # Split the line into name and birthdate
-    name, birthdate = line.strip().split(":")
+    # Split the line into name and birthdate using any whitespace characters
+    data = line.strip().split()
     
-    # Append name and birthdate to their respective lists
-    names.append(name.strip())
-    birthdates.append(birthdate.strip())
+    # Check if the line contains "... etc." entry
+    if "... etc." in line:
+        # Append "... etc." to both names and birthdates lists
+        names.append("... etc.")
+        birthdates.append("... etc.")
+    else:
+        # Join the name if it contains multiple parts
+        name = ' '.join(data[:-3])
+        # Join the birthdate if it contains multiple parts
+        birthdate = ' '.join(data[-3:])
+        
+        # Append name and birthdate to their respective lists
+        names.append(name)
+        birthdates.append(birthdate)
 
 # Print names
 print("Name")
